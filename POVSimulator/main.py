ecb38f3c84da544cbbb2dc8236b6858e9cd398dd
Valentin Mercy <mercy.valentin@gmail.com>
2021-09-27 22:35:31 +0200

    created drillHoles function

--- POVSimulator/main.py
+++ POVSimulator/main.py
@@ -1,9 +1,6 @@
-import subprocess
 from PIL import Image
 from PIL import ImageDraw
 from math import pi, sin,cos
-from subprocess import STDOUT, Popen, PIPE
-from time import sleep
 
 def drillHole(draw, centerPosition, diameter):
     """Adds a transparent hole in draw
@@ -11,7 +8,7 @@ def drillHole(draw, centerPosition, diameter):
     Args:
         draw (Draw): The PIL Draw to modify
         centerPosition (tuple): Pixel coordinates (x,y) of the hole center
-        diameter (int): Hole diameter in pixels
+        diameter (int): Hole diameter in px
     """
     upperLeftCornerCoordinates = (
         centerPosition[0]-diameter/2, centerPosition[1]-diameter/2)
@@ -20,22 +17,22 @@ def drillHole(draw, centerPosition, diameter):
     draw.ellipse([upperLeftCornerCoordinates,
                   bottomRightCornerCoordinates], fill=(0, 0, 0, 0))
 
-def drillHoles(draw, positions, diameter):
-    """Drills several transparent holes in draw
+def drillLineOfHoles(draw, positions, diameter):
+    """Drills multiple transparent aligned holes in draw
 
     Args:
         draw (Draw): The PIL Draw to modify
         positions (array): Array of tuples containing pixel coordinates (x,y) of holes centers
-        diameter (int): Holes diameter in pixels
+        diameter (int): Holes diameter in px
     """
     for holeCenter in positions:
         drillHole(draw, holeCenter, diameter)
 
-def computeHolesCenterPositions(maskDiameter, nbHoles, angle):
+def computeHolesLineCenterPositions(maskDiameter, nbHoles, angle):
   """Computes center positions of the holes to drill in mask
 
   Args:
-      maskDiameter (int): The diameter of the mask in pixels
+      maskDiameter (int): The diameter of the mask in px
       nbHoles (int): The expected number of holes
       angle (float): The angle of the diameter line where holes must be alligned
 
@@ -59,35 +56,45 @@ def computeHolesCenterPositions(maskDiameter, nbHoles, angle):
     holesPositions.append((maskDiameter/2-radial*sin(angle), maskDiameter/2-radial*cos(angle)))
   return holesPositions
 
+def drillHoles(draw, maskDiameter, nbHoles, holesDiameter, angleStep, angleMin = 0, angleMax = 180):
+  """Drills all holes on mask
+
+  Args:
+      draw (Draw): The PIL Draw to modify
+      maskDiameter (int): The diameter of the mask in px
+      nbHoles (int): The expected number of holes in each diameter line
+      diameter (int): Hole diameter in px
+      angleStep (int): The angle between two consecutive lines of holes
+      angleMin (int, optional): The angle of the first line of holes. Defaults to 0.
+      angleMax (int, optional): The angle of the last line of holes. Defaults to 180.
+  """
+  for i in range (angleMin,angleMax,angleStep):
+    angle_rad = pi/180.0 * i
+    holes = computeHolesLineCenterPositions(maskDiameter, nbHoles, angle_rad)
+    drillLineOfHoles(draw, holes, holesDiameter)
+
 # Configuration variables
 maskDiameter = 5000
 holesDiameter = 20
 nbHoles = 144
-maskFileName = 'mask.png'
-zoom = 90 #background image zoom in %
-
-# try to make the mask spin above image
-# if it doesn't work, pick colors on image on mask holes positions and build new image with them
-mask = Image.new('RGBA', (maskDiameter, )*2, (255, 0, 0, 0))
-support = Image.new('RGBA', (maskDiameter,)*2, (255,0,0,0))
-draw = ImageDraw.Draw(mask)
-
-draw.rectangle([(0, 0), (maskDiameter, maskDiameter)], 'black')
-
-for i in range (0,180,5):
-  angle_rad = pi/180.0 * i
-  holes = computeHolesCenterPositions(maskDiameter, nbHoles, angle_rad)
-  drillHoles(draw, holes, holesDiameter)
-
-mask.save(maskFileName)
-#Popen(['xdg-open', 'mask.png'], stdout=PIPE)
-
 imageFileName = 'logo_utbm.png'
 outputFileName = 'result.png'
-backgroundImage = Image.open(imageFileName)
-backgroundImage = backgroundImage.convert('RGBA')
-backgroundImageNewSize = maskDiameter*zoom//100
-backgroundImage = backgroundImage.resize((backgroundImageNewSize,)*2)
-support.paste(backgroundImage, ((maskDiameter-backgroundImageNewSize)//2,)*2)
-support.paste(mask, (0,0), mask)
-support.save(outputFileName)
+zoom = 90 #background image zoom in %
+
+if __name__ == "__main__":
+  mask = Image.new('RGBA', (maskDiameter, )*2, (255, 0, 0, 0)) # The mask is a black-filled image with transparent holes
+  support = Image.new('RGBA', (maskDiameter,)*2, (255,0,0,0)) # The support image is the main image which 
+  backgroundImage = Image.open(imageFileName) #
+
+  draw = ImageDraw.Draw(mask)
+  draw.rectangle([(0, 0), (maskDiameter, maskDiameter)], 'black') #
+
+  drillHoles(draw, maskDiameter, nbHoles, holesDiameter, 5)
+  #maskFileName = 'mask.png'
+  #mask.save(maskFileName)
+  backgroundImage = backgroundImage.convert('RGBA')
+  backgroundImageNewSize = maskDiameter*zoom//100
+  backgroundImage = backgroundImage.resize((backgroundImageNewSize,)*2)
+  support.paste(backgroundImage, ((maskDiameter-backgroundImageNewSize)//2,)*2)
+  support.paste(mask, (0,0), mask)
+  support.save(outputFileName)
