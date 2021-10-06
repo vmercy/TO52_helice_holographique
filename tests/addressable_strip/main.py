886dfcda887c6ade01e36fdaa5a189f4a619fa24
Valentin Mercy <mercy.valentin@gmail.com>
2021-10-06 15:51:58 +0200

    added delay

--- tests/addressable_strip/main.py
+++ tests/addressable_strip/main.py
@@ -5,6 +5,7 @@
 addressable_strip/main.py: Draft script aiming to test an addressable LED strip with following reference: sj-100144-102
 """
 from apa102_pi.driver import apa102
+from time import sleep
 
 strip = apa102.APA102(num_led=144, order='rgb')
 
@@ -18,5 +19,7 @@ strip.set_pixel_rgb(4, 0x0000FF)  # Blue
 strip.set_pixel_rgb(5, 0x0000FF)  # Blue
 
 strip.show()
+
+sleep(1)
 
 strip.cleanup()
