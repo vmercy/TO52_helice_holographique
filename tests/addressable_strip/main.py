c1beb9ae6a11f58c6cebdafbbb9740cf5a51597b
Valentin Mercy <mercy.valentin@gmail.com>
2021-10-06 16:33:18 +0200

    start at 500 hz

--- tests/addressable_strip/main.py
+++ tests/addressable_strip/main.py
@@ -23,8 +23,8 @@ strip.clear_strip()
 
 nbLeds = 144
 freqMax = 1000
-dureeFixe = 2
-for freq in range(1,freqMax):
+dureeFixe = 0.5
+for freq in range(500,freqMax):
   delay = (1/freq)/2
   print(freq)
   duree = dureeFixe
