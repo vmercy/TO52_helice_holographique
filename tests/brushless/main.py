e9c4cd9726ccc513152d7b5800486b08b8e94431
Valentin Mercy <mercy.valentin@gmail.com>
2021-10-11 11:21:45 +0200

    changed sleep

--- tests/brushless/main.py
+++ tests/brushless/main.py
@@ -15,7 +15,7 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 p.ChangeDutyCycle(0)
-time.sleep(2)
+time.sleep(10)
 print('Starting motor')
 try:
   while True:
