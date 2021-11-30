a5408f5276238b6c43a6c66f7301a7869b0c2ad2
Valentin Mercy <mercy.valentin@gmail.com>
2021-11-30 22:08:02 +0100

    test

--- tests/addressable_strip/colorFade.py
+++ tests/addressable_strip/colorFade.py
@@ -7,24 +7,25 @@ import sys
 strip = apa102.APA102(num_led=144, order='rgb')
 strip2 = apa102.APA102(num_led=144, order='rgb', mosi=20, sclk=21)
 
+# Helper for converting 0-255 offset to a colour tuple
+def wheel(offset, brightness):
+    # The colours are a transition r - g - b - back to r
+    offset = 255 - offset
+    if offset < 85:
+        return (255 - offset * 3, 0, offset * 3, brightness)
+    if offset < 170:
+        offset -= 85
+        return (0, offset * 3, 255 - offset * 3, brightness)
+    offset -= 170
+    return (offset * 3, 255 - offset * 3, 0, brightness)
+
+
 def colorFade(strip, colorFrom, colorTo, wait_ms=20, steps=10):
-    steps = 200
-
-    step_R = int(colorTo[0]) / steps
-    step_G = int(colorTo[1]) / steps
-    step_B = int(colorTo[2]) / steps
-    r = int(colorFrom[0])
-    g = int(colorFrom[1])
-    b = int(colorFrom[2])
-
-    for x in range(steps):
-        c = (int(r), int(g), int(b))
-        for i in range(48):
-            strip.set_pixel_rgb(i, hex(c))
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        r += step_R
-        g += step_G
-        b += step_B
+    for r in range(5):
+      for n in range(256):
+          for i in range(strip.n):
+              strip[i] = wheel(((i * 256 // strip.n) + n) & 255, 255)
+          strip.write()
+      time.sleep_ms(25)
 
 colorFade(strip, (255,0,255),(0,255,0))
