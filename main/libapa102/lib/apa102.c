4236f55891acc5593a6b09f86ce76b916fe48a8d
Valentin Mercy <mercy.valentin@gmail.com>
2021-12-10 12:44:08 +0100

    test

--- main/libapa102/lib/apa102.c
+++ main/libapa102/lib/apa102.c
@@ -79,13 +79,15 @@ void APA102_Fill(struct APA102* strip, struct APA102_Frame* led) {
     led->brightness = 31;
   }
 
-  APA102_Begin();
   for(i = 0; i < strip->n_leds; i++) {
     led_frame[0] = 0b11100000 | (0b00011111 & led->brightness);
     led_frame[1] = led->b;
     led_frame[2] = led->g;
     led_frame[3] = led->r;
 
+  }
+  APA102_Begin();
+  for(i = 0; i < strip->n_leds; i++) {
     wiringPiSPIDataRW(0, led_frame, 4);
   }
   APA102_End();
