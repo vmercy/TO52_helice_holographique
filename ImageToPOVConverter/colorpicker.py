86740ec1343cd94decc2b49cff884b257fb51e9c
Valentin Mercy <mercy.valentin@gmail.com>
2021-12-12 16:59:31 +0100

    test

--- ImageToPOVConverter/colorpicker.py
+++ ImageToPOVConverter/colorpicker.py
@@ -124,7 +124,7 @@ def savePickedColors(filename, pickedColors, nbSectors):
 
 # Configuration variables
 nbEllipsesPerDiametralLine = 2*48
-imageFileName = 'mario.png'
+imageFileName = 'batman.png'
 outputFileName = 'result_picking.png'
 nbSectors = 13 #number of displaying sectors
 angleStep = int(180/nbSectors)
@@ -144,4 +144,4 @@ if __name__ == "__main__":
   pickedColors = pickColors(pickingPoints, centeredImage)
 
   savePickedColors('/home/valentin/TO52_helice_holographique/main/libapa102/examples/logo_utbm.h', pickedColors, nbSectors)
-  renderPickedPointsPreview(outputFileName, pickedColors, 10)
+  renderPickedPointsPreview(outputFileName, pickedColors, 3)
